b10908f82ae0593d763fa561b3a8f7d386d9f6fe
triniti111 <btclovespb777@gmail.com>
2025-07-15 12:42:32 +0400

    First commit

---
+++ calculator.py
@@ -0,0 +1,8 @@
+def calculate(a, b):
+	return a + b
+
+print("Hello, this program adds two numbers")
+a = int(input("Enter first number -> "))
+b = int(input("Enter second number -> "))
+print(f'Result: {calculate(a, b)}')
+
